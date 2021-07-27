ad2caded381732b5b4b363d32a876d05c65b40bd
jay <jjhjoa2494@kaist.ac.kr>
2021-07-28 01:58:44 +0900

    django course 36.5 commit

---
+++ pragmatic/commentapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name='commentapp'
+urlpatterns=[
+    # path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+
+]
---
+++ pragmatic/commentapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.views.generic import CreateView
+
+
+class CommentCreateView(CreateView):
+    d
+
