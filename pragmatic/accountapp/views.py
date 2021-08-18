ebfdedd214609728d6479e4f9f7fb4ac2e3ca25e
jay <jjhjoa2494@kaist.ac.kr>
2021-08-18 23:57:43 +0900

    진짜 마이너한거 수정 08182358

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -10,7 +10,7 @@ from django.views.generic.list import MultipleObjectMixin
 
 from .accountupdateform import AccountUpdateForm
 from .decorator import account_ownership_required
-# from .models import HelloWorld
+
 from articleapp.models import Article
 
 has_ownership = [
@@ -42,7 +42,7 @@ has_ownership = [
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/create.html'
 
 
@@ -80,5 +80,5 @@ class AccountUpdateView(UpdateView):
     context_object_name = 'target_user'
     # form_class = UserCreationForm
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/update.html'
